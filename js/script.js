768f436238436d52b098e96f707fbff971ab0aff
Michael Gaudet <Michael.G224@gmail.com>
2017-01-25 15:44:30 -0600

    added Logo and dropdown menu's

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    $('#logo').fadeOut();
+    $('.store-hide').hide();
+    $('.social-hide').hide();
+    $('#store-toggle').on('click', function(){
+        $('.store-hide').slideToggle();
+    });
+    $('#social-toggle').on('click', function(){
+        $('.social-hide').slideToggle('left');
+    });
 });
